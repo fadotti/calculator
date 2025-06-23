c05fa16b84cc53751a9cfe0b233a11289c453523
Federico Dotti <fede.dotti.94@gmail.com>
2025-06-23 18:10:41 +0000

    Allow the user to input numbers with the keypad

---
+++ script1.js
@@ -0,0 +1,19 @@
+const rowOne = document.querySelector('#row-1');
+const rowTwo = document.querySelector('#row-2');
+const maximumDisplayLength = 21;
+
+rowTwo.textContent = '';
+
+const buttons = document.querySelectorAll('.keypad-button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const buttonType = button.classList;
+        const buttonValue = button.textContent;
+        switch(true) {
+            case Object.values(buttonType).findIndex((element) => element == 'number') > 0:
+                if(rowTwo.textContent.length < 21) {
+                    rowTwo.textContent += buttonValue;
+                }
+        }
+    })
+})
